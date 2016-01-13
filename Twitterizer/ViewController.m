ad3c34c93d5648727c2709c10a206bb269c87d39
Steven Fellows <stevenfellows@Stevens-MacBook-Pro-2.local>
2016-01-13 16:02:40 -0600

    recent changes

--- Twitterizer/ViewController.m
+++ Twitterizer/ViewController.m
@@ -8,8 +8,9 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController () <UITextViewDelegate>
 @property (weak, nonatomic) IBOutlet UITextView *textView;
+@property (weak, nonatomic) IBOutlet UILabel *label;
 
 @end
 
@@ -22,23 +23,38 @@
 - (IBAction)twitterize:(UIButton *)sender
 {
     NSString *twitterizedText = self.textView.text;
-    NSCharacterSet *characterSet = 
-    
     NSMutableString *mutableText = [NSMutableString new];
     NSInteger length = twitterizedText.length;
-    for (int i = 0; i < length; i++) {
-    NSString *character = [NSString stringWithFormat:@"%c", [twitterizedText characterAtIndex:i]];
-        if ([NSCharacterSet twitterizedText])  [NSCharacterSet characterSetWithCharactersInString:@"aeiou"];
-            
-            NSRange r;
+    for (int i = 0; i < length; i++)
+    {
+        NSString *character = [NSString stringWithFormat:@"%c", [twitterizedText characterAtIndex:i]];
+        if (![character containsString:@"a"])
+        if (![character containsString:@"e"])
+        if (![character containsString:@"i"])
+        if (![character containsString:@"o"])
+        if (![character containsString:@"u"])
+        if (![character containsString:@"A"])
+        if (![character containsString:@"E"])
+        if (![character containsString:@"I"])
+        if (![character containsString:@"O"])
+        if (![character containsString:@"U"])
+        {
+            [mutableText appendString:character];
         }
-      
-    
     }
     
-//    NSString *character = [NSString stringWithFormat:@"%c", [firstPart characterAtIndex:i - 1]];
-//    [secondPart appendString:character];
+    self.textView.text = [NSString stringWithFormat:@"%@", mutableText];
     
 }
+
+-(void)textViewDidChange:(UITextView *)textView
+{
+    NSInteger count = self.textView.text.length;
+    NSString *countValue = [NSString stringWithFormat:@"%lu", count];
+    self.label.text = countValue;
+}
+
+
+    
 
 @end
