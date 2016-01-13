9e86eb383f768a3c73d619486b6fe8f255c959ec
mouerj <joseph.mouer@icloud.com>
2016-01-13 13:54:16 -0600

    initial commit

---
+++ Twitterizer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Twitterizer
+//
+//  Created by Joseph Mouer on 1/13/16.
+//  Copyright © 2016 Joseph Mouer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
