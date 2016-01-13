00a7b3900481f0d7dd5de51640d5128e298756bf
Steven Fellows <stevenfellows@Stevens-MacBook-Pro-2.local>
2016-01-13 14:27:52 -0600

    first changes

--- Twitterizer/ViewController.m
+++ Twitterizer/ViewController.m
@@ -9,6 +9,7 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UITextView *textView;
 
 @end
 
@@ -16,12 +17,22 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (IBAction)twitterize:(UIButton *)sender
+{
+    NSString *twitterizedText = self.textView.text;
+    NSMutableString *mutableText = [NSMutableString new];
+    NSInteger length = twitterizedText.length;
+    for (int i = 0; i < length; i++) {
+        if ([twitterizedText characterAtIndex:i] == ) {
+            <#statements#>
+        }
+    }
+    
+//    NSString *character = [NSString stringWithFormat:@"%c", [firstPart characterAtIndex:i - 1]];
+//    [secondPart appendString:character];
+    
 }
 
 @end
